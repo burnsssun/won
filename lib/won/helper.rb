cafd88867d5342081f4b9a53fbd900a924f5a181
BJ KIM <burnssun@gmail.com>
2011-02-22 15:45:08 +0900

    backing up exist file

--- lib/won/helper.rb
+++ lib/won/helper.rb
@@ -8,10 +8,10 @@ module Won
       engine = :str
       template = nil
       path = nil
-      if args.first.is_a?( Symbol )
+      if args.first.is_a?( Symbol ) # file :mu, "Rakefile", :rake
         engine = args.first
         path, template = args[1],args[2]
-      else
+      else # file "hello.c", :hello
         engine = :str
         path, template = args[0],args[1]
       end
@@ -22,12 +22,13 @@ module Won
       else
         if block_given?
           out = yield 
-        else # file :erb, './base/version.rb'
+        else # file :mu, './base/version.rb'
           out = send engine, path.to_sym
         end
       end
       raise "can not render: template missing? " unless out
       fullpath = File.expand_path(path) 
+      File.rename fullpath, fullpath + '.bak' if File.exist?( fullpath )
       ::FileUtils.mkdir_p File.dirname( fullpath )
       File.open( fullpath, mode ) { |f| f.write( out ) }
     end
--- lib/won/version.rb
+++ lib/won/version.rb
@@ -1,7 +1,7 @@
 
 module Won
 
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
 
 end
 
